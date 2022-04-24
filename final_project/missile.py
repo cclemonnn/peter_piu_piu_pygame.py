47f73f558b950a7800e23df2a4360584d3c3ba29
cclemonnn <petersocool0313@gmail.com>
2022-04-24 20:03:47 +0800

    added missile.py

---
+++ final_project/missile.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Missile(Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        # missile image width: 17, height: 40
+        self.image = pygame.image.load("images/missile.png").convert_alpha()
+        # self.image.set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.vertical_speed = -15
+
+    def update(self):
+        self.rect.bottom += self.vertical_speed
+        if self.rect.bottom < 0:
+            self.kill()
--- final_project/peter_piu_piu_pygame.py
+++ final_project/peter_piu_piu_pygame.py
@@ -9,26 +9,8 @@ import settings
 from final_project.antagonists import Meteor, UFO, Monster
 from final_project.explosion import Explosion
 from final_project.ground import Ground
+from final_project.missile import Missile
 from final_project.player import Player
-
-
-
-
-class Missile(Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        # missile image width: 17, height: 40
-        self.image = pygame.image.load("images/missile.png").convert_alpha()
-        # self.image.set_colorkey((0, 0, 0))
-        self.rect = self.image.get_rect()
-        self.rect.centerx = x
-        self.rect.bottom = y
-        self.vertical_speed = -15
-
-    def update(self):
-        self.rect.bottom += self.vertical_speed
-        if self.rect.bottom < 0:
-            self.kill()
 
 
 class Viewport:
